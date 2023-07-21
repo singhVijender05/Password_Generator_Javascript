874723f416db64f2b711d7934fd2ecfea3b01c1e
singhVijender05 <kvsingh709@gmail.com>
2023-07-21 22:18:56 +0530

    Added very strong password generator function

--- password_generator.js
+++ password_generator.js
@@ -12,6 +12,7 @@ class Password {
         this.Password = password
         console.log(`Password has been set successfully for Username=${username}`)
     }
+    //utility function to generate random no between a given range
     getRandom(max, min) {
         return Math.floor(Math.random() * (max - min + 1) + min)
     }
@@ -31,9 +32,12 @@ class Password {
 
         return password
     }
+    //function to generate strong password
     generateStrongPassword(length){
+        //take only three character sets ; special characters are not included
         const CharacterSets = [Password.lowerCaseChars, Password.upperCaseChars, Password.digitChars]
         let password=''
+        //ensure it contain atleast one character of every character set
         for(let i=0;i<CharacterSets.length;i++){
           password+=CharacterSets[i][this.getRandom(0,CharacterSets[i].length)]
         }
@@ -43,7 +47,24 @@ class Password {
         }
         return password
     }
-    ResetPassword = (password) => {
+
+    //function to generate very strong password
+    generateVeryStrongPassword(length){
+        const CharacterSets = [Password.lowerCaseChars, Password.upperCaseChars, Password.digitChars, Password.specialChars]
+        let password=''
+        //ensure it contain atleast one character of every character set
+        for(let i=0;i<CharacterSets.length;i++){
+            let char_index=this.getRandom(0,CharacterSets[i].length)
+            password+=CharacterSets[i][char_index]
+        }
+        for(let i=0;i<length-CharacterSets.length;i++){
+            let set_index=this.getRandom(0,CharacterSets.length)
+            let char_index=this.getRandom(0,CharacterSets[set_index].length)
+            password+=CharacterSets[set_index][char_index]
+        }
+        return password
+    }
+    ResetPassword = (newpassword) => {
         //Implement later
         //     let response=prompt('Enter last password you remember')
         //     if(response!=this.password){
@@ -53,7 +74,7 @@ class Password {
         //     this.Password=Password
         //     alert(`Password has been successully reset for ${this.username}`)
         // }
-        this.Password = password;
+        this.Password = newpassword;
         console.log(`Password has been successully reset for ${this.username}`)
     }
 
@@ -62,3 +83,4 @@ class Password {
 User1 = new Password('Rahul', '12345678')
 console.log(User1.generateWeakPassword())
 console.log(User1.generateStrongPassword(10))
+console.log(User1.generateVeryStrongPassword(10))
